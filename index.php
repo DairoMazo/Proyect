d215ad9fa8da169d20695224649df909a8f7c95f
DairoMazo <86647219+DairoMazo@users.noreply.github.com>
2021-07-30 19:31:33 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Depressitest</title>
+    <link rel="stylesheet" href="Css/estilo1.css">
+    <script
+      src="https://kit.fontawesome.com/7e5b2d153f.js"
+      crossorigin="anonymous"
+    ></script>
+    <script defer src="index.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+
+<body>
+
+<header class="header">
+        <img class="Foto"  src="Img_Proyecto/definitivo.jpg">
+        <nav class="nav">
+          <a class="logo nav-link">Depressitest</a>
+          <button class="nav-toggle" aria-label="Abrir menú">
+            <i class="fas fa-bars"></i>
+          </button>
+          <ul class="nav-menu">
+            <li class="nav-menu-item">
+              <a href="index.php" class="nav-menu-link nav-link nav-menu-link_active">Inicio</a>
+            </li>
+            <li class="nav-menu-item">
+              <a href="Nosotros.php" class="nav-menu-link nav-link">Nosotros</a>
+            </li>
+            <li class="nav-menu-item">
+              <a href="Contacto.php" class="nav-menu-link nav-link">Contacto</a>
+            </li>
+            <li class="nav-menu-item">
+              <a href="Registro.php" class="nav-menu-link nav-link">Registrate</a>
+            </li>
+            <li class="nav-menu-item">
+                <a href="iniciar_sesion.php" class="nav-menu-link nav-link">Iniciar Sesión</a>
+              </li>
+          </ul>
+        </nav>
+    </header>
+
+    <h1 align ="center"> Hay lugares con luz, aún en tiempos muy oscuros</h1>
+
+        <p align="center">Para eso estamos nosotros, un aplicativo web que mide tus niveles de depresión y 
+            que factores la están causando, regístrate, inicia tu test gratis ahora. 
+        </p>
+
+        <br>
+        <br>
+        
+        <center><a class="Boton" href="Registro.php">Comenzar</a></center>
+
+        <br>
+        <br>
+        <br>
+        <br>
+
+        <div class="title-cards">
+            <h2>Más información</h2>
+            <hr size="4" width="50%">
+        </div>
+
+
+    <div class="container-card">
+        
+    <div class="card">
+       
+        <div class="contenido-card">
+            <h3>¿Qué es la depresión?</h3>
+            <p>Según la OMS, la depresión es una enfermedad que se caracteriza por una tristeza persitente y 
+                por la pérdida de interés en las actividades con las que normalmente se suele disfrutar.
+            </p>
+            <a href="Registro.php">Leer Más</a>
+        </div>
+    </div>
+    <div class="card">
+        
+        <div class="contenido-card">
+            <h3>¿Cómo saber si lo estoy?</h3>
+            <p>Una de las causas principales puede ser el aislamiento social.
+                Para eso estamos nosotros un aplicativo web, que mide tus niveles de 
+                depresión y que factores la causan.
+            </p>
+            <a href="Registro.php">Leer Más</a>
+        </div>
+    </div>
+    <div class="card">
+        
+            
+        <div class="contenido-card">
+            <h3>¿Cómo funciona el aplicativo web?</h3>
+            <p>Hemos agendado mas de 30 citas al mes, gracias a Depressitest hemos logrado mejorar y cambiar decisiones importantes 
+                sobre el ánimo de vida de cada persona. 
+            </p>
+            <a href="Registro.php">Leer Màs</a>
+        </div>
+    </div>
+    </div>
+
+    <br>
+    <br>
+
+    <!--Footer-->
+
+    <footer>
+        <div class="footer-content">
+            <h3>Depressitest</h3>
+            <p>Siguenos en nuestras redes sociales para más información sobre la página </p>
+            <ul class="socials">
+                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa fa-whatsapp"></i></a></li>
+                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+            </ul>
+        </div>
+        <div class="footer-bottom">
+            <p>Depressitest 2021 Depressitest. designed by <span>Dairo, Mariana, Camilo.</span></p>
+        </div>
+    </footer>
+
+   
+
+</body>
+
+
+</html>
